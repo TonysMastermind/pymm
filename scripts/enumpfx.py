5f0f8026c953f8a303f101afc7c8936b0781d2df
Antoun Kanawati <antounk@gmail.com>
2014-05-29 08:09:21 -0700

    use shell script wrapper for scripts.

--- scripts/enumpfx.py
+++ scripts/enumpfx.py
@@ -4,17 +4,35 @@ from mm import CODETABLE
 import mm.xforms as xf
 import mm.distinct as distinct
 
+import argparse
 import sys
 
+def parser():
+    p = argparse.ArgumentParser(description='Enumerate Mastermind prefixes.')
+    p.add_argument('--initial', '-i', type=int, dest='initial',
+                   help='Initial code in the prefix.',
+                   action='store', default=8)
+    p.add_argument('--maxlen', '-m', type=int, dest='maxlen',
+                   help='Maximum prefix length.',
+                   action='store', default=3)
+
+    return p
+
 def main():
-    maxlen = 3
-    if len(sys.argv) > 1:
-        maxlen = int(sys.argv[1])
+    class options(object):
+        pass
+
+    opt = options()
+    p = parser()
+    args = p.parse_args(namespace=opt)
+
+    maxlen = args.maxlen
+    first = args.initial
 
     g = distinct.PrefixGen()
     g.skip_non_reducing = True
 
-    for (p, i, d) in g.prefixes(8, maxlen):
+    for (p, i, d) in g.prefixes(first, maxlen):
         print "{}, {}, {}, {}".format(len(p), len(i), len(d), p) 
 
 if __name__ == '__main__':
