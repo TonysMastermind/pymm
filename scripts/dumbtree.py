e56a05724a941792f455fb44c4aea4317155191b
Antoun Kanawati <antounk@gmail.com>
2014-06-07 09:37:42 -0700

    removing profiling from dumbtree

--- scripts/dumbtree.py
+++ scripts/dumbtree.py
@@ -2,27 +2,11 @@ import mm
 import mm.builder as builder
 import mm.score
 
-import cProfile as profile
-import pstats
-import StringIO
-
-def doit(pr):
-    b = builder.TreeBuilder(builder.BuilderContext, mm.CODETABLE.ALL)
-    pr.enable()
-    t = b.build(9, root=8)
-    pr.disable()
-    print t.as_json_string()
-
 def main():
     mm.score.initialize()
-
-    pr = profile.Profile()
-    doit(pr)
-
-    s = StringIO.StringIO()
-    ps = pstats.Stats(pr, stream=s).strip_dirs().sort_stats('time')
-    ps.print_stats()
-    print s.getvalue()
+    b = builder.TreeBuilder(builder.BuilderContext, mm.CODETABLE.ALL)
+    t = b.build(9, root=8)
+    print t.as_json_string()
 
 if __name__ == '__main__':
     main()
